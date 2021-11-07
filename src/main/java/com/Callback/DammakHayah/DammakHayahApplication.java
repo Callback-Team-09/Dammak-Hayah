1a6f99800be7f995dfc818dbe9d50253cf210850
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-11-07 17:21:08 +0200

    init commit

---
+++ src/main/java/com/Callback/DammakHayah/DammakHayahApplication.java
@@ -0,0 +1,13 @@
+package com.Callback.DammakHayah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DammakHayahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DammakHayahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Callback/DammakHayah/DammakHayahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Callback.DammakHayah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DammakHayahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
